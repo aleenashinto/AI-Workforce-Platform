2b505f239e364641128b2d1b7ab75ce27af9a79c
aleenashinto <aleenakochumon2@gmail.com>
2026-08-20 13:44:44 +0530

    update

---
+++ packages/db/setup.js
@@ -0,0 +1,13 @@
+const postgres = require('postgres');
+const sql = postgres('postgresql://postgres:Aleena%40123%23@db.xarcuonsgcexagzevwdu.supabase.co:5432/postgres');
+async function run() {
+  try {
+    await sql`CREATE EXTENSION IF NOT EXISTS vector`;
+    console.log('Extensions enabled');
+  } catch (e) {
+    console.error(e);
+  } finally {
+    process.exit(0);
+  }
+}
+run();
